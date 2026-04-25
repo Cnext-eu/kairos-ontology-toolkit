68360f0e56403a1bafbc928958b74f4d613fab4e
Peter Van Kerkhove <peter.vankerkhove@cnext.ai>
2026-04-25 11:19:25 +0200

    Release v2.13.2

--- src/kairos_ontology/__init__.py
+++ src/kairos_ontology/__init__.py
@@ -2,7 +2,7 @@
 # Copyright 2026 Cnext.eu
 """Kairos Ontology Toolkit - Validation and projection tools for OWL/Turtle ontologies."""
 
-__version__ = "2.13.1"
+__version__ = "2.13.2"
 
 from kairos_ontology.catalog_utils import CatalogResolver, load_graph_with_catalog
 from kairos_ontology.validator import run_validation, validate_content, validate_gdpr
